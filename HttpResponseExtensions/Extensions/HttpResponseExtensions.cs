146867e5182b864d98f7c9fca4b7ecdaf87525b0
William Guo <william.guo@plexure.com>
2020-02-05 11:37:46 +1300

    Add HttpResponseExtensions

---
+++ HttpResponseExtensions/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mime;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HttpResponseExtensions.Extensions
+{
+    /// <summary>
+    /// A collection of HttpResponse extensions.
+    /// </summary>
+    public static class HttpResponseExtensions
+    {
+        /// <summary>
+        /// Serializes the object and write it to the response.
+        /// </summary>
+        /// <param name="httpResponse">The HTTP Response</param>
+        /// <param name="obj">The object</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>The write response task</returns>
+        public static async Task WriteJsonAsync(this HttpResponse httpResponse, object obj, CancellationToken cancellationToken = default)
+        {
+            httpResponse.ContentType = MediaTypeNames.Application.Json;
+            var json = JsonSerializer.Serialize(obj);
+            await httpResponse.WriteAsync(json, cancellationToken);
+        }
+    }
+}
